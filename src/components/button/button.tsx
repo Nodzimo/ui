d42fa7d196829772a3ec37a4b70742859058fb2f
Sefo Nodzimo <Sefo@Nodzimo.com>
2026-05-10 18:13:24 +0300

    refactor: Клиентский тестовый компонент кнопка

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import type { ComponentPropsWithoutRef } from 'react'
 
-export function Button(props: ComponentPropsWithoutRef<'button'>) {
-	return <button {...props}>UI kit test button</button>
+export function Button({
+	children,
+	...restProps
+}: ComponentPropsWithoutRef<'button'>) {
+	return <button {...restProps}>{children} [UI kit test button]</button>
 }
