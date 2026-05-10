4aedc1ab8ed659f16fc2d44b674cca4a52d81c65
Sefo Nodzimo <Sefo@Nodzimo.com>
2026-05-10 18:04:21 +0300

    feat: Генерация типизации

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import dts from 'unplugin-dts/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+		dts({
+			tsconfigPath: 'tsconfig.app.json',
+		}),
+	],
 	build: {
 		lib: {
 			entry: 'src/index',
