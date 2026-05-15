809964c418392945ec71f66fca17e73525cfe838
Sefo Nodzimo <Sefo@Nodzimo.com>
2026-05-15 06:41:22 +0300

    fix: Конфигурация Vite после обновления зависимостей

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="vitest/config" />
-
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import babel from '@rolldown/plugin-babel'
@@ -8,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { playwright } from '@vitest/browser-playwright'
 import dts from 'unplugin-dts/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const dirname =
